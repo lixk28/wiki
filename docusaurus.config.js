8dcb0908153dbf7d8fd865e24c265d574cf0a6b6
Snow <i@xkun.me>
2022-04-04 23:42:01 +0800

    config: change site favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/kaf.png',
+  favicon: 'img/sio.png',
   organizationName: 'lixk28', // Usually your GitHub org/user name.
   projectName: 'wiki', // Usually your repo name.
   deploymentBranch: 'gh-pages',
