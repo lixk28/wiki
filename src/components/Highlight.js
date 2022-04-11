10198ab98cb9a746a30d25a36e7755ca03943e6d
snow <i@xkun.me>
2022-04-11 17:37:08 +0800

    src/components/Highlight: disable background color and padding
    We just want to highlight the text :^)

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -4,10 +4,10 @@ export default function Highlight({children, color}) {
   return (
     <span
       style={{
-        backgroundColor: color,
-        borderRadius: '2px',
-        color: '#fff',
-        padding: '0.2rem',
+        // backgroundColor: color,
+        // borderRadius: '2px',
+        color: color,
+        // padding: '0.2rem',
       }}>
       {children}
     </span>
